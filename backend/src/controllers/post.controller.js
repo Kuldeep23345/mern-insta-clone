28974da945dae93eabeb9f9413e5ecafb997ce55
Kuldeep <kunalrajput67718@gmail.com>
2025-09-17 20:43:59 +0530

    post everything done

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -232,7 +232,44 @@ const deletePost = async (req, res) => {
   }
 };
 
+const bookmarkPost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const authorId = req.user._id;
 
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res
+        .status(404)
+        .json({ message: "Post not found", success: false });
+    }
+
+    const user = await User.findById(authorId);
+    if (user.bookmarks.includes(post._id)) {
+      await user.updateOne({ $pull: { bookmarks: post._id } });
+      await user.save();
+      return res.status(200).json({
+        type: "unsaved",
+        message: "Post removed from bookmark",
+        success: true,
+      });
+    } else {
+      await user.updateOne({ $addToSet: { bookmarks: post._id } });
+      await user.save();
+      return res.status(200).json({
+        type: "saved",
+        message: "Post  bookmarked",
+        success: true,
+      });
+    }
+  } catch (error) {
+    console.log("Error in bookmarkpost ", error);
+    return res.status(500).json({
+      message: "Internal server error in bookmarkpost",
+      success: false,
+    });
+  }
+};
 
 export {
   addNewPost,
@@ -242,5 +279,6 @@ export {
   dislikePost,
   addComment,
   getCommentOfPost,
-  deletePost
+  deletePost,
+  bookmarkPost,
 };
