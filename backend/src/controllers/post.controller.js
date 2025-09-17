c881eb78ac2140b71ce3e4733459e9e73e310255
Kuldeep <kunalrajput67718@gmail.com>
2025-09-17 18:19:44 +0530

    add comment of post,delete post done

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,6 +1,7 @@
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { Post } from "../models/post.model.js";
 import { User } from "../models/user.model.js";
+import { Comment } from "../models/comment.model.js";
 const addNewPost = async (req, res) => {
   try {
     const authorId = req.user._id;
@@ -93,4 +94,153 @@ const getUserPost = async (req, res) => {
   }
 };
 
-export { addNewPost, getAllPost };
+const likePost = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const postId = req.params.id;
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res
+        .status(404)
+        .json({ message: "post not found", success: false });
+    }
+    await post.updateOne({ $addToSet: { likes: userId } });
+    await post.save();
+
+    return res.status(200).json({ message: "Post liked ", success: true });
+  } catch (error) {
+    console.log("Error in like user ", error);
+    return res.status(500).json({
+      message: "Internal server error in like user",
+      success: false,
+    });
+  }
+};
+const dislikePost = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const postId = req.params.id;
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res
+        .status(404)
+        .json({ message: "post not found", success: false });
+    }
+    await post.updateOne({ $pull: { likes: userId } });
+    await post.save();
+
+    return res.status(200).json({ message: "Post dislike ", success: true });
+  } catch (error) {
+    console.log("Error in dislike user ", error);
+    return res.status(500).json({
+      message: "Internal server error in dislike user",
+      success: false,
+    });
+  }
+};
+
+const addComment = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const userId = req.user._id;
+    const { text } = req.body;
+    if (!text) {
+      return res
+        .status(400)
+        .json({ message: "text is required", success: false });
+    }
+
+    const post = await Post.findById(postId);
+    const comment = await Comment.create({
+      text,
+      author: userId,
+      post: postId,
+    });
+
+    await comment.populate({
+      path: "author",
+      select: "username,profilePicture",
+    });
+
+    post.comments.push(comment._id);
+    await post.save();
+
+    return res
+      .status(201)
+      .json({ message: "Comment Added", comment, success: true });
+  } catch (error) {
+    console.log("Error in add comment  ", error);
+    return res.status(500).json({
+      message: "Internal server error in add comment  ",
+      success: false,
+    });
+  }
+};
+
+const getCommentOfPost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const comments = await Comment.find({ post: postId }).populate(
+      "author",
+      "username,profilePicture"
+    );
+    if (!comments) {
+      return res
+        .status(404)
+        .json({ message: "No comments found for this post", success: false });
+    }
+    return res.status(200).json({ success: true, comments });
+  } catch (error) {
+    console.log("Error in get comment of post  ", error);
+    return res.status(500).json({
+      message: "Internal server error in get comment of post ",
+      success: false,
+    });
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const authorId = req.user._id;
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res
+        .status(404)
+        .json({ message: "Post not found", success: false });
+    }
+
+    if (post.author.toString() !== authorId) {
+      return res.status(403).json({ message: "Unauthorized" });
+    }
+    await Post.findByIdAndDelete(postId);
+    let user = await User.findById(authorId);
+    user.posts = user.posts.filter((id) => id.toString() !== postId);
+
+    await user.save();
+
+    await Comment.deleteMany({ post: postId });
+
+    return res.status(200).json({ message: "Post deleted", success: true });
+  } catch (error) {
+    console.log("Error in delete post ", error);
+    return res.status(500).json({
+      message: "Internal server error in delete post",
+      success: false,
+    });
+  }
+};
+
+
+
+export {
+  addNewPost,
+  getAllPost,
+  getUserPost,
+  likePost,
+  dislikePost,
+  addComment,
+  getCommentOfPost,
+  deletePost
+};
